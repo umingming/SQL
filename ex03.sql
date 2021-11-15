33962360c43e017c63e80c51777da36335fef80f
umingming <>
2021-11-15 18:20:57 +0900

    create table tblGit

---
+++ ex03.sql
@@ -0,0 +1,11 @@
+create table tblGit
+( 
+    seq number(3) primary key
+);
+
+drop table tblGit;
+
+select * from tblgit;
+
+insert into tblgit(seq) values (3);
+insert into tblgit(seq) values (4);
